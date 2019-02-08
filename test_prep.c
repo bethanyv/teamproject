4eb5f8340f2bd4826411d566b1c21d5a6cb235d0
Bethany <bethanyv21@gmail.com>
2019-02-08 11:03:15 -0800

    updated for a space

--- test_prep.c
+++ test_prep.c
@@ -1,8 +1,8 @@
-    #include <stdio.h>
-      #define x 2+1
-      int main(int argc, char *argv[]){
-          int y;
-          y = x * x * x;
-          printf("%d",y);
-          return 0;
+#include <stdio.h>
+#define x 2+1
+int main(int argc, char *argv[]){
+	int y;
+	y = x * x * x;
+	printf("%d\n",y);
+	return 0;
 }
