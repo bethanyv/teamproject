8643147981dcec897ca077f7d259c564a4013be1
k27star <kstecker@cs.uoregon.edu>
2019-02-21 09:28:59 -0800

    test main function from tutorial

---
+++ kristine/main.c
@@ -0,0 +1,20 @@
+#include <SFML/Window.hpp>
+
+int main() {
+    sf::Window window(sf::VideoMode(800,600), "Mywindow");
+
+    //run the program as long as the window is open
+    while (window.isOpen())
+    {
+        /*check all the window's events that were triggered 
+        since the last iteration of the loop */
+        sf::Event event;
+        while (window.pollEvent(event)) {
+
+            // "clse requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+            window.close();
+        }
+    }
+    return 0;
+}
