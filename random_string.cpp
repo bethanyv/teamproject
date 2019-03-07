7efa41d8b93a8de979d7d25062faba7e77a1c0f0
k27star <kstecker@cs.uoregon.edu>
2019-03-06 22:59:03 -0800

    Adding female_cs.txt and random_string.cpp. Hope I didn't break too many things...

--- random_string.cpp
+++ random_string.cpp
@@ -31,57 +31,58 @@ using namespace std;
 // 	std::istream *inputStream;
 // };
 
-IOUtils::IOUtils() : isFile(false), inputStream(& std::cin) {
-	// By default, read from standard input (cin)
-}
+//everything above here is in match.hpp
+// IOUtils::IOUtils() : isFile(false), inputStream(& std::cin) {
+// 	// By default, read from standard input (cin)
+// }
 
-IOUtils::~IOUtils() {
-	this->closeStream();
-}
+// IOUtils::~IOUtils() {
+// 	this->closeStream();
+// }
 
-void
-IOUtils::openStream(int argc, const char *argv[]) {
-	// If a file name is given read from file instead of standard input
-	if( argc == 2 ) { //checks for an optional input file name
-		inputStream = new std::ifstream( argv[1] );	// open the input file
-		isFile = true;
-	} else {
-		std::cout << "Please type some text to encrypt." << std::endl <<
-				"To finish, type CTRL-D on a new line (or CTRL-Z in Windows)." << std::endl;
-		inputStream = & std::cin;
-		isFile = false;
-	}
-}
+// void
+// IOUtils::openStream(int argc, const char *argv[]) {
+// 	// If a file name is given read from file instead of standard input
+// 	if( argc == 2 ) { //checks for an optional input file name
+// 		inputStream = new std::ifstream( argv[1] );	// open the input file
+// 		isFile = true;
+// 	} else {
+// 		std::cout << "Please type some text to encrypt." << std::endl <<
+// 				"To finish, type CTRL-D on a new line (or CTRL-Z in Windows)." << std::endl;
+// 		inputStream = & std::cin;
+// 		isFile = false;
+// 	}
+// }
 
-void
-IOUtils::closeStream() {
-	if (isFile && inputStream != NULL) {
-		std::ifstream *fstr = dynamic_cast<std::ifstream *>(inputStream);
-		if (fstr != NULL) {
-			if (fstr->is_open())
-				fstr->close();
-			delete fstr;
-		}
-	}
-	inputStream = NULL;
-	// if it's not a file (i.e., the stream is cin), don't close it
-}
+// void
+// IOUtils::closeStream() {
+// 	if (isFile && inputStream != NULL) {
+// 		std::ifstream *fstr = dynamic_cast<std::ifstream *>(inputStream);
+// 		if (fstr != NULL) {
+// 			if (fstr->is_open())
+// 				fstr->close();
+// 			delete fstr;
+// 		}
+// 	}
+// 	inputStream = NULL;
+// 	// if it's not a file (i.e., the stream is cin), don't close it
+// }
 
-std::string
-IOUtils::readFromStream() {
-	std::string theInput;
-	char ch;
+// std::string
+// IOUtils::readFromStream() {
+// 	std::string theInput;
+// 	char ch;
 
-	*(this->inputStream) >> std::noskipws;  // disable skipping of whitespace
-	while ( ! this->inputStream->eof() ) {
-		*(this->inputStream) >> ch;
-	    theInput += ch;
-	}
+// 	*(this->inputStream) >> std::noskipws;  // disable skipping of whitespace
+// 	while ( ! this->inputStream->eof() ) {
+// 		*(this->inputStream) >> ch;
+// 	    theInput += ch;
+// 	}
 
-	return theInput;
-}
+// 	return theInput;
+// }
 
-int IOUtils::makeFileList(string filepath, vector<string> &name) {
+int GameType::makeFileList(string filepath, vector<string> &name) {
     // vector<string> one_syl_nouns;
     string line;
     ifstream myfile (filepath);
@@ -106,7 +107,7 @@ int IOUtils::makeFileList(string filepath, vector<string> &name) {
 
 }
 
-string IOUtils::randomFileName(vector<string> &name) {
+string GameType::randomFileName(vector<string> &name) {
     string pic;
     int num = 0;
     /* initialize random seed: */
