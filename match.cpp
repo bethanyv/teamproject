b0f4cc483721a0d56d5f1e49549ba33a47e5e13f
k27star <kstecker@cs.uoregon.edu>
2019-03-08 23:40:43 -0800

    Testing random file name stuff.

--- match.cpp
+++ match.cpp
@@ -309,11 +309,11 @@ int GameType::runGame()
 	for (int i = 0; i < board.num_cards; i++) {
 		cout << "Placed card:" << board.placed_cards[i] << endl;
 	}
-	// for (int i = 0; i < board.num_cards; i++) {
-	// 	for (int j = 0; j < board.num_cards; j++) {
-	// 		cout << "matrix at [" << i << "][" << j << "] is: " << board.matrix2[i][j].file_name << endl;
-	// 	}
-	// }
+	for (int i = 0; i < board.num_cards; i++) {
+		for (int j = 0; j < board.num_cards; j++) {
+			cout << "matrix at [" << i << "][" << j << "] is: " << board.matrix[i][j].file_name << endl;
+		}
+	}
 
     board.sfml_driver();
 
