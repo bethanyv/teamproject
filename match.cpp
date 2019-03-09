e06639e188969a6756a6c2c85da2b003a2186855
k27star <kstecker@cs.uoregon.edu>
2019-03-09 01:50:43 -0800

    Working on new CardType type mismatch

--- match.cpp
+++ match.cpp
@@ -140,8 +140,8 @@ void BoardType::set_cards(string path) {
 			string file_name = pics[rand_num];
 			//file_name = file_name.c_str();
 			if (find(placed_cards.begin(), placed_cards.end(), file_name) == placed_cards.end());
-				matrix[i][j] -> file_name = file_name;
-				matrix[i][j] -> sprite.setTexture(file_name);
+				matrix[i][j].file_name = file_name;
+				matrix[i][j].sprite.setTexture(file_name);
 
 				//TODO: figure out why this isn't adding file_name to placed cards
 				placed_cards.push_back(file_name);
