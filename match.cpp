a02dac8c9ee8ade8f14ae7b95163d87aa482d29f
k27star <kstecker@cs.uoregon.edu>
2019-03-09 16:07:26 -0800

    Trying not to seg fault.

--- match.cpp
+++ match.cpp
@@ -149,7 +149,7 @@ void BoardType::set_cards(string path) {
 			// set it to this position on the board
 			// cout << "pics[rand_num] is: " << pics[rand_num] << endl;
 			// cout << "Dereferencing that we get: " << *pics[rand_num] << endl;
-			string* file_name = pics[rand_num];
+			const char* file_name = pics[rand_num];
 			// cout << "File_name is: " << *file_name << endl;
 			//file_name = file_name.c_str();
 
