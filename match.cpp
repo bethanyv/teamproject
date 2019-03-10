1fce673cd6aa36d8dc8bba66ed827662f7212756
k27star <kstecker@cs.uoregon.edu>
2019-03-09 16:51:05 -0800

    Switching back to string* from const char*

--- match.cpp
+++ match.cpp
@@ -161,7 +161,7 @@ void BoardType::set_cards(string path) {
 			}			
 
 			//if (find(placed_cards.begin(), placed_cards.end(), file_name) == placed_cards.end());
-			matrix[i][j] -> file_name = file_name;
+			matrix[i][j] -> file_name = *file_name;
 			matrix[i][j] -> sprite.setTexture(texture);
 
 			// 	//TODO: figure out why this isn't adding file_name to placed cards
@@ -179,7 +179,7 @@ void BoardType::set_cards(string path) {
 	}
 }
 
-int BoardType::makeFileList(string filepath, vector<const char*> &name) {
+int BoardType::makeFileList(string filepath, vector<string*> &name) {
     // vector<string> one_syl_nouns;
     string line;
     ifstream myfile (filepath);
@@ -191,8 +191,8 @@ int BoardType::makeFileList(string filepath, vector<const char*> &name) {
         //cout << line << endl;
             while(getline(myfile, line)){
                 //cout << "In makeFileList and saving: " << line << endl;
-				const char* new_line = new char;
-				new_line = line.c_str();
+				string* new_line = new string;
+				new_line = &line;
 				// cout << "after mallocing, we get :" << new_line << endl;
 				// cout << "Dereferencing that, we get: " << *new_line << endl;
                 name.push_back(new_line);
